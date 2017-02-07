5b961c1d6be60a3bd891325874ba288857d79845
trevor91 <trevor91@naver.com>
2017-02-07 16:36:24 +0900

    JH git test

--- opencv/JH/15.py
+++ opencv/JH/15.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def canny():
 	img = cv2.imread('test.png',0)
-	edges1 =cv2.Canny(img,50,300)
+	edges1 =cv2.Canny(img,70,300)
 	edges2 =cv2.Canny(img,100,200)
 	edges3 =cv2.Canny(img,170,200)
 
@@ -11,7 +11,7 @@ def canny():
 	cv2.imshow('canny 1',edges1)
 	cv2.imshow('canny 2',edges2)
 	cv2.imshow('canny 3',edges3)
-
+        
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
 
