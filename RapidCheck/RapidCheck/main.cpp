46270bd56dcfb6a7dda30fc849e6478c774e629d
rapidcheck <rapidcheck@gmail.com>
2017-02-23 15:19:26 +0900

    implement KCF tracking during remaining n-1 frames

--- RapidCheck/RapidCheck/main.cpp
+++ RapidCheck/RapidCheck/main.cpp
@@ -7,8 +7,11 @@
 #include <cstring>
 #include <vector>
 
-#define VIDEOFILE "videos/768x576.avi"
+// #define VIDEOFILE "videos/768x576.avi"
+#define VIDEOFILE "videos/tracking.mp4"
 #define DETECTION_PERIOD 5
+#define MAX_TRACKER_NUMS 10
+
 using namespace cv;
 
 int main(int argc, const char * argv[])
@@ -18,7 +21,9 @@ int main(int argc, const char * argv[])
 	Mat frame;
 
 	// create a tracker object
-	Ptr<Tracker> tracker = Tracker::create("KCF");
+	std::vector<Ptr<Tracker> > trackers;
+	for (int i = 0; i < MAX_TRACKER_NUMS; ++i)
+		trackers.push_back(Tracker::create("KCF"));
 
 	// create HOG descriptor
 	HOGDescriptor hog;
@@ -51,16 +56,16 @@ int main(int argc, const char * argv[])
 			break;
 
 		// implement detection using HOG every n-th frame
-		if (frameCnt++ % DETECTION_PERIOD == 0) {
+		if (frameCnt % DETECTION_PERIOD == 0) {
 
 			// reset vectors
 			found.clear();
 			found_filtered.clear();
-
+			trackers.clear();
 			
 			hog.detectMultiScale(frame, found, 0, Size(8, 8), Size(32, 32), 1.05, 2);
 			size_t i, j;
-			for (int i = 0; i<found.size(); i++)
+			for (int i = 0; i<found.size() && i < MAX_TRACKER_NUMS; i++)
 			{
 				Rect r = found[i];
 				for (j = 0; j<found.size(); j++)
@@ -70,83 +75,39 @@ int main(int argc, const char * argv[])
 					found_filtered.push_back(r);
 			}
 
-			for (i = 0; i<found_filtered.size(); i++)
+			for (i = 0; i<found_filtered.size() && i < MAX_TRACKER_NUMS; i++)
 			{
 				Rect r = found_filtered[i];
+			
 				r.x += cvRound(r.width*0.1);
 				r.width = cvRound(r.width*0.8);
 				r.y += cvRound(r.height*0.07);
 				r.height = cvRound(r.height*0.8);
+
+				Ptr<Tracker> tracker = Tracker::create("KCF");
+				tracker->init(frame, r);
+				trackers.push_back(tracker);
 				rectangle(frame, r.tl(), r.br(), Scalar(0, 255, 0), 3);
+			}
+		}
+
+		// implement tracking during remaining (n-1) frames 
+		else {
+			
+						for (int i = 0; i < found_filtered.size() && i < trackers.size(); i++) {
+				trackers[i]->update(frame, found_filtered[i]);
+				rectangle(frame, found_filtered[i], Scalar(255, 0, 0), 2, 1);
 			}
 
 		}
-
-		/*
-		// implement tracking during remaining (n-1) frames 
-		else {
-
-
-		}
-		Rect
-		// update the tracking result
-		tracker->update(frame, roi);
-		// draw the tracked object
-		cv::rectangle(frame, roi, Scalar(255, 0, 0), 2, 1);
-
-		*/
 		
-		// show image with the tracked object
+		// draw frame
 		imshow("tracker", frame);
 
 		//quit on ESC button
-		if (waitKey(100) == 27)break;
+		if (waitKey(3) == 27)break;
 
+		frameCnt++;
 	}
 	return 0;
 }
-
-/*
-void hog() {
-
-Mat img;
-namedWindow("opencv", CV_WINDOW_AUTOSIZE);
-HOGDescriptor hog;
-hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
-
-while (true)
-{
-	cap >> img;
-	if (img.empty())
-		continue;
-
-	std::vector<Rect> found, found_filtered;
-	hog.detectMultiScale(img, found, 0, Size(8, 8), Size(32, 32), 1.05, 2);
-	size_t i, j;
-	for (int i = 0; i<found.size(); i++)
-	{
-		Rect r = found[i];
-		for (j = 0; j<found.size(); j++)
-			if (j != i && (r & found[j]) == r)
-				break;
-		if (j == found.size())
-			found_filtered.push_back(r);
-	}
-
-	for (i = 0; i<found_filtered.size(); i++)
-	{
-		Rect r = found_filtered[i];
-		r.x += cvRound(r.width*0.1);
-		r.width = cvRound(r.width*0.8);
-		r.y += cvRound(r.height*0.07);
-		r.height = cvRound(r.height*0.8);
-		rectangle(img, r.tl(), r.br(), Scalar(0, 255, 0), 3);
-	}
-
-	imshow("opencv", img);
-	if (waitKey(30) == 27)
-		break;
-}
-return 0;
-}
-*/
