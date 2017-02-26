de00151717a65abcd6ee2141b17a07d4dd4dd997
trevor91 <trevor91@naver.com>
2017-02-26 21:15:41 +0900

    add Dlg

--- MFC/Resource.h
+++ MFC/Resource.h
@@ -1,20 +1,26 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by MFCtest.rc
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// MFCtest.rc에서 사용되고 있습니다.
 //
-#define IDD_ABOUTBOX				100
-#define IDP_OLE_INIT_FAILED			100
-#define IDR_MAINFRAME				128
-#define IDR_MFCtestTYPE				130
-#define ID_WINDOW_MANAGER			131
+#define IDD_FILE_NEW                    5
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_MFCtestTYPE                 130
+#define ID_WINDOW_MANAGER               131
+#define IDC_WIDTH                       1000
+#define IDC_HEIGHT                      1001
+#define IDC_IMG_TYPE                    1002
+#define IDC_Pic                         1004
+#define IDC_PICTURE_CONTROL             1004
 
-// 다음은 새 개체에 사용할 기본값입니다.
-//
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE	310
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		310
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        312
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif
