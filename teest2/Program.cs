6ec232648fdaa80da7db86723e9dd30d4956add3
trevor91 <trevor91@naver.com>
2017-03-10 20:36:29 +0900

    c# marshalling test (cs file)

---
+++ teest2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace teest2
+{
+    class Program
+    {
+        [DllImport("test1-1.dll")]
+        private static extern int ADD(int a, int b);
+        [DllImport("test1-1.dll")]
+        private static extern void PRINT();
+
+        static void Main(string[] args)
+        {
+            int result = ADD(3 , 2);
+            Console.WriteLine(result);
+            PRINT();
+        }
+    }
+}
