7b20870d4fc46fb8bb20666b898040bd2f47d6d9
이준수 <ljs93kr@gmail.com>
2017-05-10 17:21:25 +0900

    vgg16 weigths save and load test done. Now, yolo training need to start

--- python/opencv/js/darkeras/yolo/net/vgg16_net.py
+++ python/opencv/js/darkeras/yolo/net/vgg16_net.py
@@ -11,7 +11,7 @@ import yolo.config as cfg
 ## Make VGG16 networks for feature selection
 pretrained_weights_path = 'models/pretrain/vgg16_tfdim_top_weights.h5'
 img_width, img_height, img_channel = cfg.inp_size
-def yolo_vgg16_TFdim_model(is_top=False):
+def yolo_vgg16_TFdim_model(is_top=False, is_new_training=False):
 	model_vgg = Sequential()
 	model_vgg.add(Conv2D(64, (3, 3), padding='same', input_shape=(img_width, img_height,img_channel),
 					activation='relu', name='conv1_1', trainable=False))
@@ -37,14 +37,23 @@ def yolo_vgg16_TFdim_model(is_top=False):
 	model_vgg.add(Conv2D(512, (3, 3), padding='same', activation='relu', name='conv5_3', trainable=False))
 	model_vgg.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
-	if is_top:
-		model_vgg.add(Flatten())
-		model_vgg.add(Dense(4096, activation='relu', name='dense_1'))
-		model_vgg.add(Dropout(0.5))
-		model_vgg.add(Dense(4096, activation='relu', name='dense_2'))
-		model_vgg.add(Dropout(0.5))
-		model_vgg.add(Dense(1000, activation='softmax', name='dense_3'))
+	if not is_new_training:
+		if is_top:
+			model_vgg.add(Flatten())
+			model_vgg.add(Dense(4096, activation='relu', name='dense_1'))
+			model_vgg.add(Dropout(0.5))
+			model_vgg.add(Dense(4096, activation='relu', name='dense_2'))
+			model_vgg.add(Dropout(0.5))
+			model_vgg.add(Dense(1000, activation='softmax', name='dense_3'))
+		else:
+			pass
 	else:
-		pass
+		if is_top:
+			model_vgg.add(Flatten())
+			model_vgg.add(Dense(4096, activation='relu', name='new_dense_1'))
+			model_vgg.add(Dropout(0.5))
+			model_vgg.add(Dense(4096, activation='relu', name='new_dense_2'))
+			model_vgg.add(Dropout(0.5))
+			model_vgg.add(Dense(1000, activation='softmax', name='new_dense_3'))
 
 	return model_vgg
