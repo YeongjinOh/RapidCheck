64afc63a279c663cae862cac160feb8a5301265a
trevor91 <travor91@naver.com>
2017-05-24 12:47:34 +0900

    add search button(direction, color)

--- RapidCheck(front)/Form1.cs
+++ RapidCheck(front)/Form1.cs
@@ -11,8 +11,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Runtime.InteropServices;
-
-//
+using System.Drawing.Imaging;
+using System.Threading;
 //using SharpDX.MediaFoundation;
 
 namespace RapidCheck
@@ -21,6 +21,10 @@ namespace RapidCheck
     {
         private Size formSize;
         private Size pnlSize;
+
+        //검색 조건 (방향, 색상)
+        private int inputDirection = -1;
+        private int color = -1;
         public Form1()
         {
             InitializeComponent();
@@ -33,21 +37,84 @@ namespace RapidCheck
             tabPage1.Text = "검색";
             tabPage2.Text = "요약";
             tabPage3.Text = "영상";
-
-            //SharpDX.MediaFoundation.VideoDisplayControl test;
-            //SharpDX.MediaFoundation.VideoNormalizedRect temp;
-            //temp.Bottom = 30;
-            //temp.Left = 20;
-            //temp.Right = 50;
-            //temp.Top = 10;
-
-            //test.VideoWindow = tabPage2;
-            //test.SetVideoPosition(temp,null);
         }
         private void Form1_Load(object sender, EventArgs e)
         {
             formSize = new Size(this.Width, this.Height);
             pnlSize = new Size(tabPage3.Width, tabPage3.Height);
+
+            radioButton1.Text = "빨간색";
+            radioButton2.Text = "노란색";
+            radioButton3.Text = "초록색";
+            radioButton4.Text = "하늘색";
+            radioButton5.Text = "파란색";
+            radioButton6.Text = "남색";
+            radioButton7.Text = "보라색";
+            radioButton8.Text = "자주색";
+            radioButton9.Text = "검은색";
+            radioButton10.Text = "흰색";
+        }
+
+        private void direction1_Click(object sender, EventArgs e) { inputDirection = 1; }
+        private void direction2_Click(object sender, EventArgs e) { inputDirection = 2; }
+        private void direction3_Click(object sender, EventArgs e) { inputDirection = 3; }
+        private void direction4_Click(object sender, EventArgs e) { inputDirection = 4; }
+        private void direction5_Click(object sender, EventArgs e) { inputDirection = 5; }
+        private void direction6_Click(object sender, EventArgs e) { inputDirection = 6; }
+        private void direction7_Click(object sender, EventArgs e) { inputDirection = 7; }
+        private void direction8_Click(object sender, EventArgs e) { inputDirection = 8; }
+
+        string videoPath = null;
+        Thread worker;
+        private void VideoBtn_Click(object sender, EventArgs e)
+        {
+            //OpenFileDialog choofdlog = new OpenFileDialog();
+            //choofdlog.Filter = "All Files (*.*)|*.*";
+            //choofdlog.FilterIndex = 1;
+            //choofdlog.Multiselect = true;
+            //choofdlog.InitialDirectory = @"C:\videos";
+
+            //if (choofdlog.ShowDialog() == DialogResult.OK)
+            //{
+            //    videoPath = choofdlog.FileName;
+            //}
+            videoPath = @"C:\videos\tracking.mp4";
+            RapidCheck.OverlayVideo test = new RapidCheck.OverlayVideo(videoPath);
+            /////
+            //worker = new Thread(Run);
+            //worker.Start();
+        }
+        private void Run()
+        {
+            Graphics gs = tabPage2.CreateGraphics();
+            for (int i = 0; i < 66; i++)
+            {
+                System.Threading.Thread.Sleep(500);
+                //if (i >= 65) { i = 0; }
+                string filePath = @"C:\videos\66장 여기\" + i + ".bmp";
+                Bitmap back = new Bitmap(filePath);
+                gs.DrawImage(back, new Point(0, 0));
+            }
+        }
+        private void radioButton1_CheckedChanged(object sender, EventArgs e) { color = 0; }
+        private void radioButton2_CheckedChanged(object sender, EventArgs e) { color = 1; }
+        private void radioButton3_CheckedChanged(object sender, EventArgs e) { color = 2; }
+        private void radioButton4_CheckedChanged(object sender, EventArgs e) { color = 3; }
+        private void radioButton5_CheckedChanged(object sender, EventArgs e) { color = 4; }
+        private void radioButton6_CheckedChanged(object sender, EventArgs e) { color = 5; }
+        private void radioButton7_CheckedChanged(object sender, EventArgs e) { color = 6; }
+        private void radioButton8_CheckedChanged(object sender, EventArgs e) { color = 7; }
+        private void radioButton9_CheckedChanged(object sender, EventArgs e) { color = 8; }
+        private void radioButton10_CheckedChanged(object sender, EventArgs e) { color = 9; }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if(worker != null)
+            {
+                worker.Abort();
+                worker = new Thread(Run);
+                worker.Start();
+            }
         }
     }
 }
--- RapidCheck(front)/OverlayVideo.cs
+++ RapidCheck(front)/OverlayVideo.cs
@@ -9,19 +9,13 @@ using System.Drawing; //Bitmap
 using System.Drawing.Imaging;
 using MySql.Data.MySqlClient;
 using System.Data;
-
 using System.Windows.Forms;
-
-//add frame library
-//1. AviFile
-//2. SharpAvi
-//3. AForge.Video.VFW
 using Accord.Video.VFW;
 using Accord.Video.FFMPEG;
 
 namespace RapidCheck
 {
-    public class OverlayVideo
+    public partial class OverlayVideo
     {
         //private List<string> colors; //검색 조건들 이후에 추가..
         private List<Obj> ObjList; //tracking table data
@@ -34,10 +28,15 @@ namespace RapidCheck
         private List<int> trackingTableObjid;
         private List<Rectangle> trackingTableRectangle;
         private string videoPath;
-
+        private int videoWidth;
+        private int videoHeight;
+        private int frameStep;
+        private int videoid;
+        private int outputFrameNum;
         private string strConn = "Server=localhost;Database=rapidcheck;Uid=root;Pwd=1234;";
         //overlayOrders의 길이와 overlayFrames의 길이는 같아야한다??? 디펜던시가 있다
-        public OverlayVideo()
+        public OverlayVideo() { }
+        public OverlayVideo(string path)
         {
             ObjList = new List<Obj>(); //DB Table
             objectidList = new List<int>();
@@ -48,264 +47,28 @@ namespace RapidCheck
             trackingTableObjid = new List<int>();
             trackingTableFrameNum = new List<int>();
             trackingTableRectangle = new List<Rectangle>();
-            videoPath = @"C:\videos\tracking.mp4";
+            videoPath = path;
+            videoHeight = 0;
+            videoWidth = 0;
+            frameStep = 5;
+            outputFrameNum = 48;
 
-            //set (trackingTableFrameNum, trackingTableObjid, trackingTableRectangle, objectidList)
-            MessageBox.Show("sqr");
-            getMysqlObjList();
-            //set (ObjList)
-            MessageBox.Show("addObj\n" + trackingTableFrameNum.Count);
-            addObj();
-            //set Obj cropImage
-            MessageBox.Show("crop");
-            imageCrop(videoPath);
+            // step1. db에서 obj정보를 가져오고 OBJ 필드값을 세팅
+            getMysqlObjList();  //set (trackingTableFrameNum, trackingTableObjid, trackingTableRectangle, objectidList)
+            addObj();           //set (ObjList)
+            imageCrop(videoPath);//set Obj cropImage
             
-            //set overlayFrames
-            MessageBox.Show("overlay");
-            overlay();
-            MessageBox.Show("save\n" + overlayFrames.Count);
-            for (int i = 0; i < overlayFrames.Count; i++)
-            {
-                string filepath = @"C:\videos\test\" + i + ".bmp";
-                overlayFrames[i].Save(filepath);
-            }
-            MessageBox.Show("make .avi");
-            saveAviFile2();
-            /* 1. Db에서 가져온 데이터를 기준으로OBJ생성
-             * 2. image crop (this class에서 처리해야함)
-             * 3. background + cropimage
-             * -----DONE-----
-             * 4. Bitmap merge -> .avi
-             */
-        }
-        public void addObj()
-        {
-            int row = 0;
-            for (int objid = 0; objid < trackingTableObjid.Distinct().Count(); objid++)
-            {
-                Obj temp = new Obj(objid);
-                try
-                {
-                    for (; objid == trackingTableObjid[row]; row++)
-                    {
-                        temp.addCropArea(trackingTableRectangle[row]);
-                        temp.addCropPositionNum(trackingTableFrameNum[row]);
-                    }
-                }
-                catch (System.ArgumentOutOfRangeException){  }
-                ObjList.Add(temp);
-            }
-        }
-        public void addOverlayFrames(Bitmap frame)
-        {
-            overlayFrames.Add(frame);
-        }
-        public void setDefaultObjectidList(int maxObjectid)
-        {
-            for (int idx = 0; idx < maxObjectid; idx++)
-            {
-                objectidList.Add(idx);
-            }
-        }
-        public void buildOverlayOrder() // 애를 만든다. overlayOrders;
-        {
-            //일단은 모든 obj들을 0frame부터 합성
-            for (int overlayFrameNum = 0; overlayFrameNum < 100; overlayFrameNum++)
-            {
-                List<int> temp = new List<int>();
-                for (int objectidListIdx = 0; objectidListIdx < objectidList.Count; objectidListIdx++)
-                {
-                    int id = objectidList[objectidListIdx];
-                    if( !ObjList[id].emptyImage()) // image가 남아있다면
-                    {
-                        temp.Add(id);
-                    }
-                }
-                overlayOrders.Add(temp);
-            }
-        }
-        public void overlay()
-        {
-            overlayFrames = new List<Bitmap>();
-            //현재는 over algorithm에대한 방안이 없으니....frame수가 48 이상인 것들만 overlay
-            //step1. #frame >=48인 id추출 & max값 저장
-            objectidList.Clear();
-            int idCnt = trackingTableObjid.Distinct().Count();
-            for (int idIdx = 0; idIdx < idCnt; idIdx++)
-            {
-                if (ObjList[idIdx].getFrameCnt() >= 100)
-                {
-                    ObjList[idIdx].currentAreaPositionIdx = 0;
-                    ObjList[idIdx].currentImagePositionIdx = 0;
-                    objectidList.Add(idIdx);
-                }
-            }
-            //step2. 해당 frame에...그릴 id 확인
-            //set overlayOrders
-            buildOverlayOrder();
-            //step3. drawing
-            Bitmap background = new Bitmap(@"C:\videos\Background\0.bmp"); //*****Background는....0번째 프레임?
-            for (int resFrame = 0; resFrame < overlayOrders.Count; resFrame++ )
-            {
-                Bitmap BitCopy = (Bitmap)background.Clone();
-                try
-                {
-                    for (int idx = 0; idx < overlayOrders[resFrame].Count; idx++)
-                    {
-                        int id = overlayOrders[resFrame][idx];
-                        BitCopy = combinedImage(BitCopy, ObjList[id].getNextCropImage(), ObjList[id].getCropArea());
-                    }
-                    overlayFrames.Add(BitCopy);
-                }
-                catch (System.ArgumentOutOfRangeException) { }
-            }
-        }
-        public Bitmap combinedImage(Bitmap back, Bitmap front,Rectangle position)
-        {
-            try
-            {
-                if ((back != null) | (front != null))
-                {
-                    using(Graphics gr = Graphics.FromImage(back))
-                    {
-                        ColorMatrix matrix = new ColorMatrix();
-                        matrix.Matrix33 = 0.6f;
-                        ImageAttributes att = new ImageAttributes();
-                        att.SetColorMatrix(matrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
-                        gr.CompositingMode = System.Drawing.Drawing2D.CompositingMode.SourceOver;
-                        gr.DrawImage(front, position, 0,0,front.Width,front.Height, GraphicsUnit.Pixel,att);
-                    }
-                }
-                return back;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Image combined Error");
-                return null;
-            }
-        }
-        public void getMysqlObjList()
-        {
-            try
-            {
-                using (MySqlConnection conn = new MySqlConnection(strConn))
-                {
-                    DataSet ds = new DataSet();
-                    MySqlDataAdapter adapter = new MySqlDataAdapter();
-                    adapter.SelectCommand = new MySqlCommand("SELECT max(objectId) as maxid FROM rapidcheck.tracking;",conn);
-                    adapter.Fill(ds, "maxid");
-                    adapter.SelectCommand = new MySqlCommand("SELECT objectId, frameNum, x, y, width, height FROM tracking where videoId=1 ORDER BY objectId ASC, frameNum ASC", conn);
-                    adapter.Fill(ds, "data");
-                    adapter.SelectCommand = new MySqlCommand("SELECT objectId, frameNum FROM tracking where videoId=1 ORDER BY frameNum ASC", conn);
-                    adapter.Fill(ds, "objidByframe");
-                    
-                    DataTable dt = new DataTable();
-                    dt = ds.Tables["maxid"];
-                    int maxObjectid = 0;
-                    foreach (DataRow dr in dt.Rows)
-                    {
-                        maxObjectid = Convert.ToInt32(dr["maxid"]);
-                    }
-                    //set ObjectidList
-                    setDefaultObjectidList(maxObjectid);
-                    //set (trackingTableFrameNum, trackingTableObjid, trackingTableRectangle)
-                    dt = ds.Tables["data"];
-                    foreach (DataRow dr in dt.Rows)
-                    {
-                        trackingTableFrameNum.Add(Convert.ToInt32(dr["frameNum"]));
-                        trackingTableObjid.Add(Convert.ToInt32(dr["objectId"]));
-                        trackingTableRectangle.Add(new Rectangle(Convert.ToInt32(dr["x"]), Convert.ToInt32(dr["y"]), Convert.ToInt32(dr["width"]), Convert.ToInt32(dr["height"])));
-                    }
-                    //set objidByframe
-                    dt = ds.Tables["objidByframe"];
-                    foreach(DataRow dr in dt.Rows)
-                    {
-                        int tempFrame = Convert.ToInt32(dr["frameNum"]);
-                        int tempObjid = Convert.ToInt32(dr["objectId"]);
-                        if(objidByFrame.ContainsKey(tempFrame)) //Dictionary에 key가 있다면
-                        {
-                            objidByFrame[tempFrame].Add(tempObjid);
-                        }
-                        else //Dictionary에 key가 없다면
-                        {
-                            List<int> tempList = new List<int>();
-                            tempList.Add(tempObjid);
-                            objidByFrame.Add(tempFrame, tempList);
-                        }
-                    }
-                }
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("getMysqlObjList() ERROR");
-            }
-        }
-        public void imageCrop(string videoPath)
-        {
-            Accord.Video.FFMPEG.VideoFileReader reader = new Accord.Video.FFMPEG.VideoFileReader();
-            reader.Open(videoPath);
-            for (int frameNum = 0; frameNum < reader.FrameCount; frameNum++)
-            {
-                Bitmap videoFrame = reader.ReadVideoFrame();
-                if (objidByFrame.ContainsKey(frameNum))
-                {
-                    foreach(int objid in objidByFrame[frameNum])
-                    {
-                        Rectangle temp = new Rectangle();
-                        temp = ObjList[objid].getCropArea();
+            //step2. 오버레이 할 id를 선별한다
+            setObjidList(); //set #frame >=48인 id추출 & max값 저장
 
-                        ObjList[objid].addCropImage(videoFrame.Clone(temp, videoFrame.PixelFormat));
-                    }
-                }
-                videoFrame.Dispose();
-            }
-            reader.Close();
-        }
-        public void saveAviFile()
-        {
-            // instantiate AVI writer, use WMV3 codec
-            AVIWriter writer = new AVIWriter("h264");
-            //AVIWriter writer = new AVIWriter("wmv3");
-            // create new AVI file and open it            
-            writer.Open(@"C:\videos\output.avi", 960, 720);
-            // create frame image
-            //Bitmap image = new Bitmap(320, 240);
+            //step3. 오버레이 할 id를 기준으로 어떤 id가 어떤 구간에 그릴지를 설정한다
+            buildOverlayOrder(); //set overlayOrders
 
-            for (int i = 0; i < 100; i++)
-            {
-                // update image
-                //image.SetPixel(i, i, Color.Red);
-                // add the image as a new frame of video file
-                writer.AddFrame(overlayFrames[i]);
-            }
-            writer.Close();
-        }
-        public void saveAviFile2()
-        {
-            VideoFileWriter writer = new VideoFileWriter();
-            writer.Open(@"C:\videos\testAvi.avi", 960, 720, 20,VideoCodec.H264);
-            for(int i = 0 ; i < 100; i ++)
-            {
-                writer.WriteVideoFrame(overlayFrames[i]);
-            }
-            writer.Close();
+            //step4. overlay
+            overlay(); //set overlayFrames
+
+            //step5. save .Avi file
+            saveAviFile();
         }
     }
 }
-
-
-// video play
-// 0, 1, 2, 3
-// objectId = 0 ~ (numOfPedestrians-1)
-//int numOfPedestrians = 
-//Obj objects[numOfPedestrians];
-
-//foreach (DataRow dr in dt.Rows)
-//                    {
-//            int objectId = Convert.ToInt32(dr["objectId"]);
-//            Obj object= objects[objectId];
-//            int x = Convert.ToInt32(dr["x"]), y = Convert.ToInt32(dr["y"]), width = Convert.ToInt32(dr["width"]), height = Convert.ToInt32(dr["height"]);
-//            object.addCropArea(x, y, width, height);
-//                        index += 1;
-//                    }
-//                }
