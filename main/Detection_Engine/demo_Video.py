4476f7cb26055ffaf4bfa24661a1d50f7987d188
Junsu <ljs93kr@gmail.com>
2017-06-12 15:25:44 +0900

    mydata100000 base -> video_439532 steps8000.h5 very nice work

--- main/Detection_Engine/demo_Video.py
+++ main/Detection_Engine/demo_Video.py
@@ -18,26 +18,27 @@ from utils.help import DB_Helper, DB_Item
 import os
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--videoId', type=int, help='VideoId for detecting works', required=True)
-args = parser.parse_args()
-# db.delete()
-if args.videoId:
-	videoId = args.videoId
-	print("video id : ", args.videoId)
-	# TODO : query video path from mysql
-	db = DB_Helper()
-	db.open(table_name='file')
-	row = db.select(table_name='file', condition={'videoId':videoId})
-	video_path = row['videoPath']
-	frameSteps = row['frameSteps']
+# parser = argparse.ArgumentParser()
+# parser.add_argument('--videoId', type=int, help='VideoId for detecting works', required=True)
+# args = parser.parse_args()
+# # db.delete()
+# if args.videoId:
+# 	videoId = args.videoId
+# 	print("video id : ", args.videoId)
+# 	# TODO : query video path from mysql
+# 	db = DB_Helper()
+# 	db.open(table_name='file')
+# 	row = db.select(table_name='file', condition={'videoId':videoId})
+# 	video_path = row['videoPath']
+# 	frameSteps = row['frameSteps']
 #exit()
 K.set_image_dim_ordering('th')
 
 is_freeze = True
 # weigths_path = 'models/train/yolo-2class-complete.h5'
 # weigths_path = os.path.join(cfg.model_folder, cfg.model_name) + '-steps8000.h5'
-weigths_path = 'models/train/yolo-2class-mydata-tracking-cell14-steps44000.h5'
+# weigths_path = 'models/train/yolo-2class-mydata-tracking-cell14-steps44000.h5'
+weigths_path = 'models/train/yolo-2class-cell14-mydata100000/only-video_439532-steps8000.h5'
 # weigths_path = 'models/train/'+cfg.model_name+'-complete.h5'
 # weigths_path = 'models/train/yolo-2class-mydata-3video-steps5000.h5'
 test_threshold = 0.4
@@ -49,7 +50,8 @@ model = yolo_tiny_THdim_model(is_freeze)
 model.load_weights(weigths_path)
 #model.summary()
 
-
+video_path = os.path.join('test', 'my_testset', 'video_439532.mp4')
+frameSteps = 2
 frameNum = 0
 items = []
 cap = cv2.VideoCapture(video_path)
@@ -94,8 +96,8 @@ try:
 except Exception:
 	print("Exception Occured")
 finally:
-	db.close()
-	print("DB Closed in Finally..")
+	# db.close()
+	# print("DB Closed in Finally..")
 	cap.release()
 	cv2.destroyAllWindows()
 
